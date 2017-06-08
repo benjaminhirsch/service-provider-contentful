cf5ea726736988cc99b0282d97b48cadf567cdcf
Benjamin Hirsch <mail@benjaminhirsch.net>
2017-06-08 22:49:36 +0200

    Added an simple example configuration

---
+++ example/sample-config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'contentful' => [
+        'my-servicename' => [
+            'token' => '',              // Your access token
+            'spaceId' => '',            // Your space id
+            'defaultLocale' => 'de-de',
+            'preview' => false
+        ],
+        'my-servicename-preview' => [
+            'token' => '',              // Your preview access token
+            'spaceId' => '',            // Your space id
+            'defaultLocale' => 'de-de',
+            'preview' => true
+        ]
+    ]
+];
